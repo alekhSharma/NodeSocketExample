bb219f4f059d8acf1b96b1f3a7be9d7a89b7776a
alekhSharma <alekh.s0302@gmail.com>
2018-01-22 23:05:08 +0530

    Add sfdx

--- server.js
+++ server.js
@@ -13,12 +13,25 @@ const server = express()
 
 const io = socketIO(server);
 
+const sfdx = require('sfdx-node');
+
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 
   socket.on('showTime',function(){
-  	setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+  			//authorize a dev hub
+			sfdx.auth.webLogin({
+			    setdefaultdevhubusername: true,
+			    setalias: 'HubOrg'
+			})
+			.then(function(){
+			  //push source
+			  return sfdx.source.push();  
+			})
+			.then(function(){
+			  console.log('Source pushed to scratch org');  
+		});
   })
 });
 
